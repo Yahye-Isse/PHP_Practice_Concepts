adc61c6ae3f5b8656b77b8402a40943246aa1f86
Yahye Ali Isse <119488800+Yahye-Isse@users.noreply.github.com>
2026-09-15 17:19:30 +0300

    Update Index.php

--- Week1/Index.php
+++ Week1/Index.php
@@ -7,5 +7,22 @@
 </head>
 <body>
     
+    //Tags Php 
+    <?php
+    //Echo - Display the output
+    echo "Welcome to PHP";
+    //use prenthesis is optional
+    echo ("Welcome to PHP");
+    //print also use the output
+    print "Welcome to Php";
+    // use prenthesis is optional
+    print ("Welcome to Php");
+
+    //Echo to display to argument without presenthesis 
+    echo "Yahye" , "Ali"
+
+    ?>
+
+    
 </body>
 </html>
